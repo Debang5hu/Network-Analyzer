297147627d80dcf4e472968d5e73eb7efdad2eb7
Debangshu <114200360+Debang5hu@users.noreply.github.com>
2023-10-29 11:55:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
-#!/usr/bin/env python
+#!/bin/bash/env python
+# _*_ coding:utf-8_*_
 
 #supports live packert capturing!
-#usage:  sudo python3 main.py -h
+#usage: sudo python3 testmain.py wlan0
 
-#sudo chmod +x main.py
-#to run ./main.py --capture
 
 
 try:
-    import pcap  #for capturing packets
-    import sys
-    import pyshark
-    #from scapy.all import *
-    from pyfiglet import figlet_format
+    import pcap
+    from scapy.all import *
+    from scapy.utils import wrpcap  #for writting the captured packets into a pcap file
+    import os
     import threading
-    import getopt
+    from ipblock import IPBLOCK    #custom module for blocking IP address
 except:
     pass
 
@@ -27,89 +25,129 @@ YELLOW = "\033[1;33m"
 BLUE = "\033[0;34m"
 PURPLE = "\033[0;35m"
 
+#  __Initialising Global Values__
 
-#listing all the available interfaces of the device
-def ListeningInterfaces():
-    for x in pcap.findalldevs():
-        print(f"{RED}Interface --> {WHITE} {x}")
+#max request sent to target
+UTMOST = 1000
+#to count the number of packets
+PACKET_COUNTER = {}
+#the IP to check whether it's been attacked or not!
+IPTOSAVE = '192.168.1.1'
+VULNERABLE_PORTS_TO_DOS = ['21','22','25','53','80','443','8080']  #ftp,ssh,smtp,dns,http,https,http/https
+frame_number = 0 #frame number (i.e packet number)
 
-
-def saveip(ip1,ip2,dst_port):
-    with open('ip.txt','a+') as fh:
-        payload = f'source_ip : {ip1},destination_ip: {ip2},destination_port: {dst_port} \n'
-        fh.write(payload)
-
-#to analyze the captured packets
-def CapturePacket():
-    n = interface
-    CapturedPacket = pyshark.LiveCapture('any')
-    CapturedPacket.sniff(timeout=10)
-
-    for captured in CapturedPacket:
-        try:
-            for captured in CapturedPacket:
-                frame_number = captured.number
-                protocol = captured.transport_layer
-
-                if 'IP' in captured:
-                    source_ip = captured.ip.src
-                    destination_ip = captured.ip.dst
-                else:
-                    source_ip = "N/A"
-                    destination_ip = "N/A"
-
-                if captured.transport_layer:
-                    source_port = captured[captured.transport_layer].srcport
-                    destination_port = captured[captured.transport_layer].dstport
-
-                else:
-                    source_port = "N/A"
-                    destination_port = "N/A"
+#regex
+#IP_PATTERN = r'\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b'
 
 
-                print(f"{RED}Frame Number: {WHITE}{frame_number} || {BLUE}Source IP: {WHITE}{source_ip} || {YELLOW}Destination IP: {WHITE}{destination_ip} || {RED}Protocol: {WHITE}{protocol} || {BLUE}Source Port: {WHITE}{source_port} || {PURPLE}Destination Port: {WHITE}{destination_port}", end='\n')
-                
-                #marking(source_ip,destination_ip)
-                saveip(source_ip,destination_ip,destination_port)
-        except:
-            pass
+
+#to check whether the program is running as sudo or not
+def run_as_sudo():
+    if os.geteuid() == 0:
+        pass
+    else:
+        exit('[+] Run The Program with Sudo permission!')
 
 
-if __name__ == '__main__':
+
+#to analyze the captured packets and to analyze DOS
+def CapturePacket(packet):
+    global frame_number 
+    try:
+        if 'IP' in packet:
+            source_ip = packet['IP'].src
+            destination_ip = packet['IP'].dst
+
+            if 'TCP' in packet:
+                protocol = "TCP"
+                source_port = packet['TCP'].sport
+                destination_port = packet['TCP'].dport
+        
+            elif 'UDP' in packet:
+                protocol = "UDP"
+                source_port = packet['UDP'].sport
+                destination_port = packet['UDP'].dport
+            
+            else:
+                protocol = "Other"
+                source_port = "N/A"
+                destination_port = "N/A"
+
+
+            frame_number += 1 
+
+            print(f"{RED}Frame Number: {WHITE}{frame_number} || {BLUE}Source IP: {WHITE}{source_ip} || {YELLOW}Destination IP: {WHITE}{destination_ip} || {RED}Protocol: {WHITE}{protocol} || {BLUE}Source Port: {WHITE}{source_port} || {PURPLE}Destination Port: {WHITE}{destination_port}", end='\n')
+
+            #logging the captured packets in a pcap file
+            wrpcap("packet.pcap", packet, append=True)
+
+
+
+            #for DOS detection:
+            # Count the number of packets with the same source IP
+
+            for p in packet:
+                if 'IP' in p:
+                    if source_ip in PACKET_COUNTER and destination_port in VULNERABLE_PORTS_TO_DOS:
+                        PACKET_COUNTER[source_ip] += 1
+                    else:
+                        PACKET_COUNTER[source_ip] = 1
     
+            # Check if any IP address has sent too many packets
+            for ip, count in PACKET_COUNTER.items():
+
+                #checking the number of packets being sent to destination IP,if they are verified the IP causing trouble will get blocked
+                if count > UTMOST and destination_ip == IPTOSAVE :  # Adjust this UTMOST value
+                    #IPBLOCK.block(ip) #will block the ip
+                    print(f'[!] Possible {RED}DoS{WHITE} attack detected --> {RED}{ip}{WHITE} attacked {RED}{IPTOSAVE}{WHITE} with {RED}{count}{WHITE} packets on port {RED}{destination_port}{WHITE}!')
+                    print(f'[#] {ip} Blocked Succesfully')
+        
+    except:
+        pass
+
+
+
+
+def forpacketcapture():
+    packet = sniff(prn = CapturePacket,iface = interface,store = False)
+
+
+if __name__  == '__main__':
     if sys.hexversion >= 0x03080000:
         
-        #banner
-        print(figlet_format('Packet Analyzer', font = 'digital' ),end = '\n')
-            
+        #printing the banner
+        print('''
++-+-+-+-+-+-+ +-+-+-+-+-+-+-+-+
+|P|a|c|k|e|t| |A|n|a|l|y|z|e|r|
++-+-+-+-+-+-+ +-+-+-+-+-+-+-+-+
+                    - @Debang5hu
+''')
+        
+        #checking
+        run_as_sudo()
 
+        #interface = 'wlan0'
 
-        #cli input
+        #cli inputs
         try:
             
-            arguments=sys.argv[1:]
-            args,null=getopt.getopt(arguments,"c:i:",["capture=","interface="])
+            #interface in which the packet capturing will take place (eth0/wlan0)
+            interface = sys.argv[1]
+            
+            #using threading concept
+            t1 = threading.Thread(target = forpacketcapture,daemon=True) #using daemon so that runs in background too
 
-            for x,y in args:
-                if x in ['-c','--capture']:
-                    global interface
-                    interface = y
-                    #analyzing the packet
-                    t1 = threading.Thread(target = CapturePacket)
+            #starting the thread
+            t1.start()
 
-                    # start the threads
-                    t1.start()
-
-                    # join the main thread
-                    t1.join()
-                
-                if x in ['-i','--interface']:
-                    if y == 'all':
-                        ListeningInterfaces()
-   
+            #join 
+            t1.join()
+        
         except:
-            print('\nUsage: [(./main.py -c {interface_name}) or (./main.py --capture {interface_name})] for capturing network packets')
-            print('\t[(./main.py -i all) or (./main.py --interface all)] for checking available network interfaces')
-                
+            print('\n[+] Usage: [sudo python3 main.py {interface_name}] for capturing network packets')
+
     else:
         sys.exit('[+] Required Python Version > 3.8!')
+
+
+
